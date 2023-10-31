9a9b6129488aa08d16e629122c2758f10969c7b8
afonsomateus21 <mateusdeoliveira1111@gmail.com>
2023-10-31 12:42:06 -0300

    fix: alteracao na cor do header e no logotipo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { About } from "./components/About"
 import { NavBar } from "./components/NavBar"
 
-function App() {
+export function App() {
   return (
-    <main className="h-screen w-screen bg-blue-950">
+    <div className="h-screen flex flex-col bg-blue-950">
       <NavBar />
-    </main>
+
+      <main className="flex-grow mt-32">
+        <About />
+      </main>
+    </div>
   )
 }
 
-export default App
---
+++ src/components/About.tsx
@@ -0,0 +1,7 @@
+export function About() {
+  return (
+    <section className=''>
+      <h1 className="text-white">Olá, sou um desenvolvedor full stack</h1>
+    </section>
+  );
+}
