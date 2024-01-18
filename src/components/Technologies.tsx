e441d33ec924ef10635f5f76868628943859655c
Afonso Mateus <afonsomateus.dev@gmail.com>
2024-01-18 20:26:21 -0300

    fix: reorganizacao das imagens da secao tecnologias

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,19 +1,6 @@
-export function Technologies() {
-  const Links = [
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg",
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg",
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original-wordmark.svg",
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg",
-    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg"
-  ]
+import { ImageLinks } from "../constants/links";
 
+export function Technologies() {
   return (
     <section id="technologies" className="bg-blue-50 p-16 h-screen">
       <div className="w-full h-full lg:w-4/5 my-0 mx-auto flex flex-col items-center justify-center">
@@ -22,7 +9,7 @@ export function Technologies() {
         <div className='flex justify-center'>
           <div className='grid grid-rows-4 md:grid-rows-3 grid-flow-col gap-10 p-10'>
             {
-              Links.map( (link, index) => 
+              ImageLinks.map( (link, index) => 
                 <div key={index} className="w-16 h-16 md:w-28 md:h-28">
                   <img src={link} />
                 </div>  
--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,6 +1,21 @@
-export const Links =[
+export const Links = [
   {name: "INÍCIO", link: "#banner"},
   {name:"SOBRE", link:"#about"},
   {name:"TECNOLOGIAS", link:"#technologies"},
   {name:"CONTATO", link:"#contact"},
 ];
+
+export const ImageLinks = [
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nestjs/nestjs-plain-wordmark.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original-wordmark.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg"
+]
